6af299cb1a70a358eeb4ce06de993133a39ab343
Administrator <abhi_sri_amity6@yahoo.com>
2017-06-27 22:25:09 +0530

    Base and Derived Class Created

--- InheritanceDemo/Program.cs
+++ InheritanceDemo/Program.cs
@@ -6,10 +6,71 @@ using System.Threading.Tasks;
 
 namespace InheritanceDemo
 {
-    class Program
+    class TwoDShape
     {
-        static void Main(string[] args)
+        double pri_height;
+        double pri_width;
+
+        // implementing property
+        public double height
         {
+            get
+            {
+                return pri_height;
+            }
+            set
+            {
+                pri_height = value;
+            }
+        }
+
+        public double width
+        {
+            get
+            {
+                return pri_width;
+            }
+            set
+            {
+                pri_width = value;
+            }
         }
     }
+    class Triangle : TwoDShape
+    {
+        string Style;
+
+        public string StyleValue
+        {
+            get
+            {
+                return Style;
+            }
+            set
+            {
+                Style = value;
+            }
+
+        }
+            public double Area()
+        {
+            return width * height / 2;
+        }
+        // Display a triangle's style.
+        public void ShowStyle()
+        {
+            Console.WriteLine("Triangle is " + Style);
+        }
+
+    }
+
+    class Demo
+    {
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
 }
