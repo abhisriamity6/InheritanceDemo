8258f2df347ac36362ae6f7692f8b1fc4955ca0d
Administrator <abhi_sri_amity6@yahoo.com>
2017-06-27 22:30:04 +0530

    completed

--- InheritanceDemo/Program.cs
+++ InheritanceDemo/Program.cs
@@ -35,6 +35,13 @@ namespace InheritanceDemo
                 pri_width = value;
             }
         }
+
+        public void ShowDim()
+        {
+            Console.WriteLine("Width and height are " +
+            width + " and " + height);
+        }
+
     }
     class Triangle : TwoDShape
     {
@@ -52,7 +59,7 @@ namespace InheritanceDemo
             }
 
         }
-            public double Area()
+      public double Area()
         {
             return width * height / 2;
         }
@@ -69,6 +76,28 @@ namespace InheritanceDemo
 
         static void Main(string[] args)
         {
+
+            Triangle t1 = new Triangle();
+            Triangle t2 = new Triangle();
+            t1.width = 4.0;
+            t1.height = 4.0;
+            t1.StyleValue = "isosceles";
+            t2.width = 8.0;
+            t2.height = 12.0;
+            t2.StyleValue = "right";
+            Console.WriteLine("Info for t1: ");
+            t1.ShowStyle();
+            t1.ShowDim();
+            Console.WriteLine("Area is " + t1.Area());
+            Console.WriteLine();
+            Console.WriteLine("Info for t2: ");
+            t2.ShowStyle();
+            t2.ShowDim();
+            Console.WriteLine("Area is " + t2.Area());
+
+            Console.Read();
+
+
 
         }
     }
